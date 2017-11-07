aee02d1ce74bc10d4d6cc9b90be8706b0a4f60a5
Sky Xu <skyrocks88xmz@gmail.com>
2017-11-07 12:30:08 -0800

    finish first arch slider UIPath

---
+++ goodPattern/goodPattern/CirclePath.swift
@@ -0,0 +1,50 @@
+//
+//  CirclePath.swift
+//  goodPattern
+//
+//  Created by Sky Xu on 11/7/17.
+//  Copyright © 2017 Sky Xu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CirclePath: UIView {
+    let shapeLayer = CAShapeLayer()
+    var value: Float = 0 {
+        didSet {
+            drawPath()
+        }
+    }
+    
+    func drawPath() {
+        shapeLayer.strokeEnd = CGFloat(value)
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.addSublayer(shapeLayer)
+        let path = UIBezierPath()
+        let cx = bounds.width / 2
+        let cy = bounds.height / 2
+        let c = CGPoint(x: cx, y: cy)
+//        M_PI is a double so we need to convert it to float
+        let pi2 = CGFloat(Double.pi * 2)
+        let start = pi2 * 2 / 8
+        let end = pi2 * 9 / 8
+        
+        path.addArc(withCenter: c, radius: cx, startAngle: start, endAngle: end, clockwise: true)
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 10
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeEnd = CGFloat(value)
+//       line dash with 4 pint of line, 2 p of gap
+        shapeLayer.lineDashPattern = [4, 2]
+    }
+//    this required init allows us to initialize circlepath class from storyboard
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ goodPattern/goodPattern/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  goodPattern
+//
+//  Created by Sky Xu on 11/7/17.
+//  Copyright © 2017 Sky Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var cp: CirclePath!
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        cp.value = (sender as! UISlider).value
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let rect = CGRect(x: 20, y: 40, width: 200, height: 200)
+        cp = CirclePath(frame: rect)
+        view.addSubview(cp)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
